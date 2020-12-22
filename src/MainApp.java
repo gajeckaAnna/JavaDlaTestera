6413d87aecbed21f85ed7abcbd582aa92d44059f
gajeckaAnna <gajecka.anna@gmail.com>
2020-12-22 21:53:15 +0100

    Added first Hello World

---
+++ src/MainApp.java
@@ -0,0 +1,10 @@
+public class MainApp {
+
+    public static void main(String[] args) {
+        //APP'S START
+        //psvm + Tab
+
+        System.out.println("Hello World");
+        //sout + Tab
+    }
+}
