5bbccbf090705d9c2059ddd5ef50864817bb43c3
gajeckaAnna <gajecka.anna@gmail.com>
2020-12-23 15:08:33 +0100

    Added variables.java

--- src/variables.java
+++ src/variables.java
@@ -1,11 +1,17 @@
 public class variables {
 
     public static void main(String[] args) {
+//        liczby całkowite
         System.out.println(10);
+//        liczby zmiennoprzecinkowe
         System.out.println(10.5);
+//        tekst
         System.out.println("Text in Java");
+//        znak
         System.out.println('a');
+//        prawda
         System.out.println(true);
+//        fałsz
         System.out.println(false);
     }
 }
