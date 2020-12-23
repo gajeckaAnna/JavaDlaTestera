1660568eea7174216fcbac3434f2ba37910dd0f9
gajeckaAnna <gajecka.anna@gmail.com>
2020-12-23 15:01:01 +0100

    Added variables.java

---
+++ src/variables.java
@@ -0,0 +1,11 @@
+public class variables {
+
+    public static void main(String[] args) {
+        System.out.println(10);
+        System.out.println(10.5);
+        System.out.println("Text in Java");
+        System.out.println('a');
+        System.out.println(true);
+        System.out.println(false);
+    }
+}
