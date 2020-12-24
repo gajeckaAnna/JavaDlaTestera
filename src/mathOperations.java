b28c343bca32a0beca7d677b881fe34e8002855b
gajeckaAnna <gajecka.anna@gmail.com>
2020-12-24 23:48:01 +0100

    Added mathOperations.java

---
+++ src/mathOperations.java
@@ -0,0 +1,12 @@
+public class mathOperations {
+
+    public static void main(String[] args) {
+        System.out.println(10 + 10);
+        System.out.println(10 - 10);
+        System.out.println(10 * 10);
+        System.out.println(10 / 10);
+
+        System.out.println(10 / 4); // = 2
+        System.out.println(10.0 / 4.0); // = 2.5
+    }
+}
