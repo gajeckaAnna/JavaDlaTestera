14d86a89bcd63057051ad2ea7a2d0be41853127f
gajeckaAnna <gajecka.anna@gmail.com>
2020-12-25 20:32:10 +0100

    Added ifology.java

---
+++ src/ifology.java
@@ -0,0 +1,22 @@
+public class ifology {
+
+    public static void main(String[] args) {
+        int numOne = 1;
+        int numTwo = 2;
+
+        if(numOne < numTwo){
+            System.out.println("Number one is less than number two.");
+        } else {
+            System.out.println("Number one is greater than number two");
+        }
+
+        int numThree = 3;
+        int numFour = 4;
+
+        if(numThree > numFour){
+            System.out.println("Number three is less than number four.");
+        } else {
+            System.out.println("Number four is greater than number three ");
+        }
+    }
+}
