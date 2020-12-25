34422071c4193a755506987144eb76c628fb8dfc
gajeckaAnna <gajecka.anna@gmail.com>
2020-12-26 00:52:58 +0100

    creating objects

--- src/MainApp.java
+++ src/MainApp.java
@@ -6,5 +6,24 @@ public class MainApp {
 
         System.out.println("Hello World");
         //sout + Tab
+
+        // type of object [name of the class] + name == new object
+        User ania = new User();
+        ania.firstName = "Ania";
+        ania.lastName = "Gajecka";
+        ania.email = "gajecka.anna@gmail.com";
+        ania.age = 32;
+        ania.isAdult = true;
+        System.out.println(ania.firstName);
+
+        User bartek = new User();
+        bartek.firstName = "Bartek";
+        bartek.lastName = "Brown";
+        bartek.email = "bartek@brown.pl";
+        bartek.age = 30;
+        bartek.isAdult = true;
+        System.out.println(bartek.firstName);
+        System.out.println(bartek.age);
+
     }
 }
