6d6c290c2d0c2c9c0a8fc79dda24f0d9b44257d1
gajeckaAnna <gajecka.anna@gmail.com>
2020-12-25 23:06:45 +0100

    edited HomeworkOne - full sentences

--- src/HomeworkOne.java
+++ src/HomeworkOne.java
@@ -1,8 +1,8 @@
 public class HomeworkOne {
     public static void main(String[] args) {
 //      1. 4 int variables
-//      2. Check if the sum of two first is even or odd
-//      3. Check if the sum of third and four is even or odd
+//      2. Check if the sum of two first ints is even or odd
+//      3. Check if the sum of third and four ints is even or odd
 //      4. Check if both sums are even or odd
 
         int numOne = 34;
@@ -10,23 +10,20 @@ public class HomeworkOne {
         int numThree = 42;
         int numFour = 16;
 
-        if ((numOne + numTwo) % 2 == 0) {
-            System.out.println("Even number");
-        } else {
-            System.out.println("Odd number");
-        }
+        if ((numOne + numTwo) % 2 == 0)
+            System.out.println("The sum of " + numOne + " and " + numTwo + " is even." );
+        else
+            System.out.println("The sum of " + numOne + " and " + numTwo + " is odd." );
 
-        if ((numThree + numFour) % 2 == 0) {
-            System.out.println("Even number");
-        } else {
-            System.out.println("Odd number");
-        }
 
-        if ((numOne + numTwo) % 2 == 0 && (numThree + numFour) % 2 == 0) {
-            System.out.println("Even numbers");
-        } else {
-            System.out.println("Odd numbers");
-        }
+        if ((numThree + numFour) % 2 == 0)
+            System.out.println("The sum of " + numThree + " and " + numFour + " is even.");
+        else
+            System.out.println("The sum of " + numThree + " and " + numFour + " is odd.");
 
-    }
+        if ((numOne + numTwo) % 2 == 0 && (numThree + numFour) % 2 == 0)
+            System.out.println("Both sums are even.");
+        else
+            System.out.println("Both sums are odd.");
+        }
 }
