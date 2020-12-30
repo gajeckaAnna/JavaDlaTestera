a22406d4687a5762356085b8beddcb47b3f735c8
gajeckaAnna <gajecka.anna@gmail.com>
2020-12-31 00:56:48 +0100

    Updating all the methods' name

--- src/BugMain.java
+++ src/BugMain.java
@@ -1,15 +1,19 @@
 public class BugMain {
     public static void main(String[] args) {
         Bug beetle = new Bug("This is the bug description", "gajecka.anna@gmail.com", -2);
-        beetle.getAllInfo();
+        beetle.showAllBugInfo();
         beetle.getBugPriority();
         System.out.println(beetle.bugPriority);
         beetle.closeBug();
-        beetle.getAllInfo();
+        beetle.showBugStatus();
+        beetle.showAllBugInfo();
 
         Bug ladybug = new Bug("This is another bug description", "tim@bo.be", 6);
-        ladybug.getAllInfo();
+        ladybug.showAllBugInfo();
         ladybug.getBugPriority();
         System.out.println(ladybug.bugPriority);
+
+        Bug cockroach = new Bug("That is another bug!", "vincent@mun.be",3);
+        cockroach.showAllBugInfo();
     }
 }
