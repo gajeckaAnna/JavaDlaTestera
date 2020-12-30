2e9f34b3dcfda5fde8251dfa824955604339c31b
gajeckaAnna <gajecka.anna@gmail.com>
2020-12-31 00:38:58 +0100

    Changing methods' names, adding descriptions to print

--- src/Bug.java
+++ src/Bug.java
@@ -1,39 +1,45 @@
 public class Bug {
 
-        String bugDescription;
-        String bugReportedBy;
-        int bugPriority;
-        String bugStatus;
+    String bugDescription;
+    String bugReportedBy;
+    int bugPriority;
+    String bugStatus;
 
-        Bug(String bugDescription, String bugReportedBy, int bugPriority){
-            this.bugDescription = bugDescription;
-            this.bugReportedBy = bugReportedBy;
+    Bug(String bugDescription, String bugReportedBy, int bugPriority) {
+        this.bugDescription = bugDescription;
+        this.bugReportedBy = bugReportedBy;
 
-            if (bugPriority < 1) {
-                this.bugPriority = 1;
-            } else if (bugPriority > 5){
-                this.bugPriority = 5;
-            } else {
-                this.bugPriority = bugPriority;
-            }
-            this.bugStatus = "open";
+        if (bugPriority < 1) {
+            this.bugPriority = 1;
+        } else if (bugPriority > 5) {
+            this.bugPriority = 5;
+        } else {
+            this.bugPriority = bugPriority;
         }
 
-        void getAllInfo() {
-            System.out.println(bugDescription + "; " + bugReportedBy + "; " + bugPriority + "; " + bugStatus);
-        }
-        void getBugReportedBy() {
-            System.out.println(bugReportedBy);
-        }
-        void getBugStatus(){
-            System.out.println(bugStatus);
-        }
-        void closeBug(){
-            this.bugStatus = "closed";
-        }
-        int getBugPriority(){
-            return bugPriority;
-        }
+        this.bugStatus = "open";
     }
+
+    void showAllBugInfo() {
+        System.out.println("Bug description: " + bugDescription + "; reported by: " + bugReportedBy +
+                "; bug priority: " + bugPriority + "; Bug status: " + bugStatus + ".");
+    }
+
+    void showBugReportedBy() {
+        System.out.println("Reported by: " + bugReportedBy);
+    }
+
+    void showBugStatus() {
+        System.out.println("Bug status: " + bugStatus);
+    }
+
+    void closeBug() {
+        this.bugStatus = "closed";
+    }
+
+    int getBugPriority() {
+        return bugPriority;
+    }
+}
 
 
