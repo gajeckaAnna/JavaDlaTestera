c8a1378e0164fc70bd3264456ffef2d1168878cb
gajeckaAnna <gajecka.anna@gmail.com>
2021-01-03 00:58:26 +0100

    Fixing code, testing new methods

--- src/BugMain.java
+++ src/BugMain.java
@@ -1,19 +1,36 @@
+import model.Bug;
+
 public class BugMain {
     public static void main(String[] args) {
         Bug beetle = new Bug("This is the bug description", "gajecka.anna@gmail.com", -2);
         beetle.showAllBugInfo();
-        beetle.getBugPriority();
-        System.out.println(beetle.bugPriority);
+        System.out.println(beetle.getBugPriority());
         beetle.closeBug();
         beetle.showBugStatus();
         beetle.showAllBugInfo();
 
         Bug ladybug = new Bug("This is another bug description", "tim@bo.be", 6);
         ladybug.showAllBugInfo();
-        ladybug.getBugPriority();
-        System.out.println(ladybug.bugPriority);
+        System.out.println(ladybug.getBugPriority());
+
 
         Bug cockroach = new Bug("That is another bug!", "vincent@mun.be",3);
         cockroach.showAllBugInfo();
+
+        cockroach.setBugDescription("Too small");
+        System.out.println(cockroach.getBugDescription());
+        cockroach.setBugDescription("This is a longer bug description.");
+        System.out.println(cockroach.getBugDescription());
+
+        cockroach.setBugReportedBy("vincent!mun.be");
+        System.out.println(cockroach.getBugReportedBy());
+        cockroach.setBugReportedBy("vincent@mun.com");
+        System.out.println(cockroach.getBugReportedBy());
+
+        cockroach.setBugPriority(-1);
+        cockroach.setBugPriority(6);
+        cockroach.setBugPriority(4);
+        System.out.println(cockroach.getBugPriority());
+
     }
 }
