7afdba5df2238f504fb4afbeb1fa37564f083bca
gajeckaAnna <gajecka.anna@gmail.com>
2021-01-04 22:51:46 +0100

    one dimensional arrays testTeam

--- src/Arrays.java
+++ src/Arrays.java
@@ -9,5 +9,29 @@ public class Arrays {
         String[] names = new String[5];
 //         values: ["Ania", "Tamara", "Marta", "Ula", "Ola"]
 //         index:   [0,       1,        2,      3   ,   4]
+        System.out.println(names); // [Ljava.lang.String;@1e643faf
+        System.out.println(names[0]); // null
+
+        String[] testTeam = new String[6];
+        testTeam[0] = "Ania";
+        testTeam[1] = "Ingrid";
+        testTeam[2] = "Dave";
+        testTeam[3] = "Vincent";
+        testTeam[4] = "Bart";
+        testTeam[5] = "Raf";
+        testTeam[6] = "Fabienne";
+
+        System.out.println(testTeam[0]);
+        System.out.println(testTeam[1]);
+        System.out.println(testTeam[2]);
+        System.out.println(testTeam[3]);
+        System.out.println(testTeam[4]);
+        System.out.println(testTeam[5]);
+        System.out.println(testTeam[6]);
+
+        System.out.println(testTeam[7]); // Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException:
+        // Index 6 out of bounds for length 6 at Arrays.main(Arrays.java:22)
+
+
     }
 }
