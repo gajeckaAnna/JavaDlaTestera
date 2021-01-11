81583d7873c42f109ff8fba8aeeb45b43f5530e7
gajeckaAnna <gajecka.anna@gmail.com>
2021-01-11 22:38:04 +0100

    While loops

---
+++ src/While.java
@@ -0,0 +1,19 @@
+public class While {
+    public static void main(String[] args) {
+
+        int i = 10;
+
+//        while (expression) {
+//            statement(s)
+//        }
+
+        while (i < 20) {
+            System.out.println(i);
+            i++;
+        }
+        while (i > 20) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
