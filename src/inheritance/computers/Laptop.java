6151d9575c28c2319e8e321a51f9f863b8bd3912
gajeckaAnna <gajecka.anna@gmail.com>
2021-01-21 21:51:14 +0100

    Laptop & PC extends Computer.java

--- src/inheritance/computers/Laptop.java
+++ src/inheritance/computers/Laptop.java
@@ -1,63 +1,8 @@
 package inheritance.computers;
 
-public class Laptop {
-
-    private String name;
-    private String type;
-    private int hdd;
-    private int ram;
-    private boolean state;
+public class Laptop extends Computer {
 
     public Laptop(String name, String type, int hdd, int ram) {
-        this.name = name;
-        this.type = type;
-        this.hdd = hdd;
-        this.ram = ram;
-        this.state = false;
+        super(name, type, hdd, ram);
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public int getHdd() {
-        return hdd;
-    }
-
-    public int getRam() {
-        return ram;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public void setHdd(int hdd) {
-        this.hdd = hdd;
-    }
-
-    public void setRam(int ram) {
-        this.ram = ram;
-    }
-
-    public void switchOn() {
-        state = true;
-    }
-
-    public void switchOff() {
-        state = false;
-    }
-
-    public boolean getState() {
-        return state;
-    }
-
 }
