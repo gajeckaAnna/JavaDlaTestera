ba19a331f3947a2d8c4e408af7612c76220711fa
gajeckaAnna <gajecka.anna@gmail.com>
2021-01-24 19:22:37 +0100

    Homework nr 5 - overriding switchOn method in PC.java

--- src/inheritance/computers/PC.java
+++ src/inheritance/computers/PC.java
@@ -2,13 +2,28 @@ package inheritance.computers;
 
 public class PC extends Computer {
 
+    private boolean isPowerSupply;
+
     public PC(String name, String type, int hdd, int ram) {
         super(name, type, hdd, ram);
+        isPowerSupply = false;
     }
 
     @Override
     public void switchOn() {
-        System.out.println("Checking power supply");
-        super.switchOn();
+        System.out.println("Checking power supply.");
+        if (isPowerSupply == true) {
+            System.out.println("♫I've got the power!♫ PC is on!");
+            super.switchOn();
+        } else {
+            System.out.println("No power supply detected. Please, plug me in!");
+        }
     }
+
+    public void togglePowerSupply() {
+        isPowerSupply = !isPowerSupply;
+        System.out.println("Toggled power supply to " + isPowerSupply + "!");
+    }
+
 }
+
