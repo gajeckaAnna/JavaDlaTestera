9a9d7fe23a7a60ad2e431670df5188658db3b8a9
gajeckaAnna <gajecka.anna@gmail.com>
2021-02-07 23:52:56 +0100

    Interface Music

---
+++ src/inheritance/computers/Music.java
@@ -0,0 +1,31 @@
+package inheritance.computers;
+
+public interface Music {
+
+//    We can’t create instance(interface can’t be instantiated) of interface but we can make reference of it that
+//    refers to the Object of its implementing class.
+//    All the fields are public, static, and final.
+    public static final String NAME = "MUSIC";
+
+//    All the methods are public and abstract.
+    void playMusic();
+    void pauseMusic();
+    void stopMusic();
+
+//    Java 8: we can now add default implementation for interface methods.
+//    This default implementation has special use and does not affect the intention behind interfaces.
+    default void sayHelloFromMusic() {
+        System.out.println("Hello from default!");
+    }
+
+//    Java 9: interfaces can contain following also static, private & private static methods.
+    static String getName() {
+        privateMethod();
+        return NAME;
+    }
+
+    private static void privateMethod() {
+        System.out.println("Hello from private static!");
+    }
+
+}
