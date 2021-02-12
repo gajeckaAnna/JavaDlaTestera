2385e00e0d3da6c87b611b6e00f293ed1b56756d
gajeckaAnna <gajecka.anna@gmail.com>
2021-02-12 22:15:35 +0100

    Boxing

---
+++ src/Boxing.java
@@ -0,0 +1,17 @@
+public class Boxing {
+
+    public static void main(String[] args) {
+
+        Byte b;
+        Short s;
+        Integer i;
+        Long l;
+        Float f;
+        Double d;
+        Character c;
+        Boolean bl;
+
+        int number = 10;
+        Integer intNumber = new Integer(10);
+    }
+}
