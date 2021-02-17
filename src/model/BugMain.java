c76c98d5e0e53186395defa5361ae4b0d43fa018
gajeckaAnna <gajecka.anna@gmail.com>
2021-02-18 00:26:58 +0100

    adding BugReporters in BugMain

--- src/model/BugMain.java
+++ src/model/BugMain.java
@@ -6,8 +6,6 @@ public class BugMain {
     public static void main(String[] args) {
 
         BugReporter ania = new BugReporter("Ania", "Gajecka", "gajecka.anna@gmail.com");
-//        BugReporter vincent = new BugReporter("Vincent", "Marvolo", "vincent.marvolo@fod.be");
-//        BugReporter bart = new BugReporter("Bart", "Esbeek", "bart@fod.be");
 
         Bug beetle = new Bug("This is the bug description", 2, ania);
         beetle.showAllBugInfo();
@@ -17,28 +15,36 @@ public class BugMain {
         beetle.showBugReportedBy();
         beetle.closeBug();
 
-//        Bug ladybug = new Bug("This is another bug description", 6, vincent);
-//        ladybug.showAllBugInfo();
-//        System.out.println(ladybug.getBugPriority());
-//
-//
-//        Bug cockroach = new Bug("That is another bug!", 3, vincent);
-//        cockroach.showAllBugInfo();
-//
-//        cockroach.setBugDescription("Too small");
-//        System.out.println(cockroach.getBugDescription());
-//        cockroach.setBugDescription("This is a longer bug description.");
-//        System.out.println(cockroach.getBugDescription());
-//
+        BugReporter bart = new BugReporter("Bart", "Esbeek", "bart.at.fod.be");
+        System.out.println(bart);
+
+        Bug polyphaga = new Bug("Polyphaga", 7, bart);
+        polyphaga.showAllBugInfo();
+
+        Bug jewelBeetle = new Bug("", -4, bart);
+        jewelBeetle.showAllBugInfo();
+
+        BugReporter vincent = new BugReporter("Vincent", "Marvolo", "vincent.marvolo@fod.be");
+        Bug ladybug = new Bug("This is another bug description", 6, vincent);
+        ladybug.showAllBugInfo();
+        ladybug.getBugPriority();
+
+        Bug cockroach = new Bug("That is another bug!", 3, vincent);
+        cockroach.showAllBugInfo();
+
+        cockroach.setBugDescription("Too small");
+        System.out.println(cockroach.getBugDescription());
+        cockroach.setBugDescription("This is a longer bug description.");
+        System.out.println(cockroach.getBugDescription());
+
 //        cockroach.setBugReportedBy("vincent!mun.be");
 //        System.out.println(cockroach.getBugReportedBy());
 //        cockroach.setBugReportedBy("vincent@mun.com");
 //        System.out.println(cockroach.getBugReportedBy());
-//
-//        cockroach.setBugPriority(-1);
-//        cockroach.setBugPriority(6);
-//        cockroach.setBugPriority(4);
-//        System.out.println(cockroach.getBugPriority());
+
+        cockroach.setBugPriority(-1);
+        cockroach.setBugPriority(6);
+        cockroach.setBugPriority(4);
 
     }
 }
