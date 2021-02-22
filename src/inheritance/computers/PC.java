edf56ec15ecb843555a6723bff8a0359f6bace83
gajeckaAnna <gajecka.anna@gmail.com>
2021-02-22 22:18:38 +0100

    Changing methods concerning volumeUp & volumeDown

--- src/inheritance/computers/PC.java
+++ src/inheritance/computers/PC.java
@@ -34,48 +34,44 @@ public class PC extends Computer {
         return volumeDown(1);
     }
 
+    private int turningUp(int adjustVolumeLevel) {
+        if (volumeLevel >= 100) {
+            System.out.println("Oh, you sweet summer child. I set my volume level to 100!");
+            return volumeLevel = 100;
+        }
+        return adjustVolumeLevel;
+    }
+
+    private int checkNegativeVolume(int adjustVolumeLevel) {
+        if (adjustVolumeLevel < 0) {
+            System.out.println("Ugh, no. We don't appreciate negative vibes around here.");
+            return adjustVolumeLevel = 0;
+        }
+        return adjustVolumeLevel;
+    }
+
+    private int turningDown(int adjustVolumeLevel) {
+        if (volumeLevel <= 0) {
+            System.out.println("Too much. I set my volume level to 0!");
+            return volumeLevel = 0;
+        }
+        return adjustVolumeLevel;
+    }
+
     @Override
     public int volumeUp(int adjustVolumeLevel) {
-        if (adjustVolumeLevel < 0) {
-            adjustVolumeLevel = 0;
-            System.out.println("Ugh, no. We don't appreciate negative vibes around here.");
-            return volumeLevel;
-        } if (adjustVolumeLevel == 0) {
-            System.out.println(adjustVolumeLevel + "? Boy, oh boy. My volume level did not change.");
-            return volumeLevel;
-        }
+        adjustVolumeLevel = checkNegativeVolume(adjustVolumeLevel);
         volumeLevel += adjustVolumeLevel;
-        if (volumeLevel > 100) {
-            System.out.println(adjustVolumeLevel + "? Oh, you sweet summer child. I set my volume level to 100!");
-            return volumeLevel = 100;
-        } if (volumeLevel == 100) {
-            System.out.println("Ring, a ding, ding. It's already a hundred, dear.");
-            return volumeLevel;
-        }
-        System.out.println(adjustVolumeLevel + "? My new volume level is up to " + volumeLevel + ".");
+        turningUp(adjustVolumeLevel);
         return volumeLevel;
     }
 
     @Override
-    public int volumeDown(int adjustVolumeLevel){
-        if (adjustVolumeLevel < 0) {
-            adjustVolumeLevel = 0;
-            System.out.println("Ugh, no. We don't appreciate negative vibes around here.");
-            return volumeLevel;
-        }
+    public int volumeDown(int adjustVolumeLevel) {
+        adjustVolumeLevel = checkNegativeVolume(adjustVolumeLevel);
         volumeLevel -= adjustVolumeLevel;
-        if (volumeLevel < 0) {
-            System.out.println(adjustVolumeLevel + "? I have standards I don't plan on lowering for anybody. " +
-                    "I set my volume level to 0!");
-            return volumeLevel = 0;
-        }
-        if (adjustVolumeLevel == 0) {
-            System.out.println(adjustVolumeLevel + "? Guurl. My volume level did not change.");
-            return volumeLevel;
-        } else {
-            System.out.println(adjustVolumeLevel + "? My new volume level is down to " + volumeLevel + ".");
-            return volumeLevel;
-        }
+        turningDown(adjustVolumeLevel);
+        return volumeLevel;
     }
 
     public int getVolumeLevel() {
