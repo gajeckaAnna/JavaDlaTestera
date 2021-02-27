575526517db1504cacee8a3868a195bdf71dbdae
gajeckaAnna <gajecka.anna@gmail.com>
2021-02-27 21:13:48 +0100

    Collecctions app

---
+++ src/collections/MainApp.java
@@ -0,0 +1,23 @@
+package collections;
+
+import java.util.ArrayList;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> testTeam = new ArrayList<>();
+        testTeam.add("Fabienne");
+        testTeam.add("Vincent");
+        testTeam.add("Ania");
+        testTeam.add("Sasa");
+        testTeam.add("Bart");
+        testTeam.add("Raf");
+        testTeam.add("Ingrid");
+        testTeam.add("Dave");
+        testTeam.add("Pieter");
+        testTeam.add("David");
+        testTeam.add("Nicolas");
+        testTeam.add("Marc");
+    }
+}
