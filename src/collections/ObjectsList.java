e07ba873c918c9ed242f66f00d3dbc3f56ac1f68
Anna Gajecka <gajecka.anna@gmail.com>
2021-02-28 21:58:11 +0100

    creating an objects list - users

---
+++ src/collections/ObjectsList.java
@@ -0,0 +1,16 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObjectsList {
+
+    public static void main(String[] args) {
+
+        List<User> users = new ArrayList<>();
+        users.add(new User("Dayla", "DeMon", 18));
+        users.add(new User("TamaNocnaMara", "Doe", 17));
+        users.add(new User("Martantula", "Doe", 17));
+
+    }
+}
