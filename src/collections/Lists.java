493903930bbe03d688aae8d865929d1b53455a09
Anna Gajecka <gajecka.anna@gmail.com>
2021-03-02 21:47:29 +0100

    created LinkedList example

---
+++ src/collections/Lists.java
@@ -0,0 +1,28 @@
+package collections;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Lists {
+
+    public static void main(String[] args) {
+
+        // dynamic array
+        List<String> technicolor = new LinkedList<>();
+
+        //elements (nodes) are linked using pointers and addresses
+        technicolor.add("blue");
+        technicolor.add("green");
+        technicolor.add("yellow");
+        technicolor.add("red");
+
+        System.out.println(technicolor);
+
+        technicolor.add(1, "turquoise");
+        System.out.println(technicolor);
+
+        technicolor.add(1, "navy blue");
+        System.out.println(technicolor);
+
+    }
+}
