1fca15213d7385242177b44d8f5e7ef56f4d532c
Anna Gajecka <gajecka.anna@gmail.com>
2021-03-10 19:18:42 +0100

    Object's set

---
+++ src/collections/ObejctsSet.java
@@ -0,0 +1,23 @@
+package collections;
+
+import java.util.*;
+
+public class ObejctsSet {
+
+    public static void main(String[] args) {
+
+        Set<User> users = new HashSet<>();
+        users.add(new User("Dayla", "DeMon", 18));
+        users.add(new User("TamaNocnaMara", "Doe", 17));
+        users.add(new User("Martantula", "Doe", 17));
+        users.add(new User("Andżej", "Tęcza", 27));
+        users.add(new User("Marceli", "Wisienka", 14));
+        users.add(new User("Stefania", "Grabarz", 89));
+
+        System.out.println(users.size());
+
+        for (User user : users) {
+            System.out.println(user.getFirstName() + " " + user.getLastName());
+        }
+    }
+}
--- src/collections/User.java
+++ src/collections/User.java
@@ -1,5 +1,7 @@
 package collections;
 
+import java.util.Objects;
+
 public class User {
 
     String firstName;
@@ -43,5 +45,18 @@ public class User {
                 ", lastName='" + lastName + '\'' +
                 ", age=" + age +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, age);
     }
 }
